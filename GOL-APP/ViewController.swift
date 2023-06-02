0a248009547f411893a0415aba9deded41abec80
yoosion030 <yoosioff@gmail.com>
2023-06-02 17:45:31 +0900

    Initial Commit

---
+++ GOL-APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GOL-APP
+//
+//  Created by 유시온 on 2023/06/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
